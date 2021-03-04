d43f4927944285392ab5f609cecde0ba60c62de3
sashkalubimov@yandex.ru <sashkalubimov@yandex.ru>
2021-03-04 17:33:34 +0300

    html css done

---
+++ source/app/main.js
@@ -0,0 +1,14 @@
+'use strict';
+const form = document.querySelector('#chat__form');
+const input = document.querySelector('.inputArea__input');
+
+class Message{
+  constructor(author,text){
+    this.author = author;
+    this.text = text;
+  }
+}
+
+form.addEventListener('submit',(e)=>{
+
+});
